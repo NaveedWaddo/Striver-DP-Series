d3241b5b8ffd49373e5bc4d494f92286037c93d8
Naveed Waddo <109472792+NaveedWaddo@users.noreply.github.com>
2023-09-07 00:55:34 +0530

    Create Recursion_for_fibonacci_Number.cpp

---
+++ 1. Introduction to DP/Recursion_for_fibonacci_Number.cpp
@@ -0,0 +1,22 @@
+//Recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int n){
+  if(n <= 1)
+    return n;
+
+  return f(n-1) + f(n-2);
+}
+
+int main(){
+  int n;
+  cin>>n;
+
+  cout<<f(n);
+
+  return 0;
+}
+//T.C : O(n)
+//S.C : O(n)
